cb7ac4f38862fb6169f5e293da1ed0c413b36066
dimmageiras <dimmageiras@gmail.com>
2025-07-24 23:11:52 +0300

    Refactor styles and components: Update button and input styles in root.scss, enhance PageTitle component to accept additional props and class names, and adjust margins in Staff and Treatments pages for improved layout consistency.

--- src/components/PageTitle/PageTitle.tsx
+++ src/components/PageTitle/PageTitle.tsx
@@ -1,13 +1,24 @@
-import type { JSX } from "react";
+import classNames from "classnames";
+import type { HTMLAttributes, JSX, RefAttributes } from "react";
 
 import styles from "./PageTitle.module.scss";
 
-interface PageTitleProps {
+interface PageTitleProps
+  extends HTMLAttributes<HTMLHeadingElement>,
+    RefAttributes<HTMLHeadingElement> {
   pageTitle: string;
 }
 
-const PageTitle = ({ pageTitle }: PageTitleProps): JSX.Element => {
-  return <h2 className={styles["page-title"]}>{pageTitle}</h2>;
+const PageTitle = ({
+  className,
+  pageTitle,
+  ...props
+}: PageTitleProps): JSX.Element => {
+  return (
+    <h2 className={classNames(styles["page-title"], className)} {...props}>
+      {pageTitle}
+    </h2>
+  );
 };
 
 export { PageTitle };
