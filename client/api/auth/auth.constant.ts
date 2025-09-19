98cac4b1d9b9aefc722c5f00abab5bd125a1cad2
dimmageiras <dimmageiras@gmail.com>
2025-09-19 13:05:34 +0000

    feat: Implement authentication service and constants for signin, signup, and verification endpoints

---
+++ client/api/auth/auth.constant.ts
@@ -0,0 +1,12 @@
+import { AUTH_ENDPOINTS } from "@shared/constants/api.constant";
+import { USER_BASE_URL } from "@shared/constants/base-urls.const";
+
+const { SIGNIN, SIGNUP, VERIFY } = AUTH_ENDPOINTS;
+
+const AUTH_QUERY_KEYS = Object.freeze({
+  SIGNIN: [USER_BASE_URL, SIGNIN] as const,
+  SIGNUP: [USER_BASE_URL, SIGNUP] as const,
+  VERIFY: [USER_BASE_URL, VERIFY] as const,
+} as const);
+
+export { AUTH_QUERY_KEYS };
