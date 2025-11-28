22faed1661c8b085201cc2c063b8ee524fb2a947
dimmageiras <dimmageiras@gmail.com>
2025-11-28 11:29:04 +0000

    chore: update dependencies and configuration for improved functionality
    - Upgraded several packages in package.json and pnpm-lock.yaml, including @tanstack/react-query (5.90.11), zod (4.1.13), and @types/react (19.2.7).
    - Updated the package manager to pnpm@10.24.0 for better package management.
    - Refactored Vite configuration to enhance plugin settings and improve development experience.
    - Added a new recommendation for the Aikido extension in VSCode for better security practices.
    - Adjusted axios configuration to use import.meta.env.SSR for server-side rendering checks.

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,16 @@ import pluginBabel from "vite-plugin-babel";
 import pluginChecker from "vite-plugin-checker";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-const viteConfig = defineConfig(({ mode }) => {
-  const IS_DEVELOPMENT = mode === "development";
-  const HAS_DEV_TOOLS = process.env.VITE_APP_ALL_DEV_TOOLS === "true";
-  const HAS_RRDT = process.env.VITE_APP_RRDT === "true";
-
+const viteConfig = defineConfig(() => {
   return {
     plugins: [
-      HAS_DEV_TOOLS && HAS_RRDT && reactRouterDevTools(),
+      reactRouterDevTools({
+        includeInProd: {
+          client: false,
+          devTools: false,
+          server: false,
+        },
+      }),
       reactRouter(),
       pluginBabel({
         babelConfig: {
@@ -28,24 +30,24 @@ const viteConfig = defineConfig(({ mode }) => {
         include: ["./client/**/*"],
       }),
       tsConfigPaths(),
-      IS_DEVELOPMENT &&
-        pluginChecker({
-          eslint: {
-            dev: {
-              logLevel: ["error"],
-            },
-            lintCommand: `eslint . \
+      pluginChecker({
+        enableBuild: false,
+        eslint: {
+          dev: {
+            logLevel: ["error"],
+          },
+          lintCommand: `eslint . \
     --report-unused-disable-directives \
     --max-warnings 0 \
     --rule "no-console: ['error', { allow: ['error', 'info', 'warn'] }]" \
     --rule "react-hooks/exhaustive-deps: off"`,
-            useFlatConfig: true,
-          },
-          overlay: {
-            initialIsOpen: false,
-          },
-          typescript: true,
-        }),
+          useFlatConfig: true,
+        },
+        overlay: {
+          initialIsOpen: false,
+        },
+        typescript: true,
+      }),
     ],
     server: {
       hmr: {
