ef13dc3a1fe383fecf285a507335f0c50ac099a4
dimmageiras <dimmageiras@gmail.com>
2025-12-17 12:21:35 +0000

    refactor: simplify environment variable validation logic
    - Removed the handleValidationSuccess function to streamline the validation process.
    - Directly returned from validateEnv upon successful parsing of environment variables, enhancing code clarity.

--- server/helpers/env-var.helper.ts
+++ server/helpers/env-var.helper.ts
@@ -1,13 +1,5 @@
-import { MODE, MODES } from "../../shared/constants/root-env.constant.ts";
 import { ZodUtilsHelper } from "../../shared/helpers/zod.helper.ts";
 import { envSchema } from "../schemas/env-var.schema.ts";
-import { PinoLogHelper } from "./pino-log.helper.ts";
-
-const handleValidationSuccess = (): void => {
-  if (MODE !== MODES.TYPE_GENERATOR) {
-    PinoLogHelper.log.info("✅ Required environment variables are set...");
-  }
-};
 
 const validateEnv = (): void => {
   const { formatError } = ZodUtilsHelper;
@@ -15,8 +7,6 @@ const validateEnv = (): void => {
   const result = envSchema.safeParse(process.env);
 
   if (result.success) {
-    handleValidationSuccess();
-
     return;
   }
 
