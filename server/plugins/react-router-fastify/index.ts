11bd64b2cf498575537f53949956d69692139aaa
dimmageiras <dimmageiras@gmail.com>
2026-02-13 14:46:53 +0000

    refactor: simplify exports in react-router-fastify plugin
    - Removed unused type exports from the react-router-fastify plugin, streamlining the code and improving clarity.

--- server/plugins/react-router-fastify/index.ts
+++ server/plugins/react-router-fastify/index.ts
@@ -1,14 +1,6 @@
 import type { GetLoadContextFunction } from "@server/plugins/react-router-fastify/types/load-context.type";
-import type {
-  CreateRequestHandlerFactory,
-  CreateRequestHandlerOptions,
-} from "@server/plugins/react-router-fastify/types/plugin-factory.type";
 
 import { createRequestHandler } from "./react-router-fastify.plugin.ts";
 
-export type {
-  GetLoadContextFunction,
-  CreateRequestHandlerFactory,
-  CreateRequestHandlerOptions,
-};
+export type { GetLoadContextFunction };
 export { createRequestHandler };
