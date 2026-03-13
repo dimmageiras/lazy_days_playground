ff5e09317fd4eeddd36fa5e0e5c5960911e11211
dimmageiras <dimmageiras@gmail.com>
2026-03-13 07:59:13 +0000

    refactor(routes): simplify authentication route handling
    - Removed specific signin and signup path parameters from the authentication route definition.
    - Updated the route to accept a generic step parameter, enhancing flexibility in route handling.
    - Cleaned up imports by removing unused constants related to authentication paths.

--- client/routes/public.route.ts
+++ client/routes/public.route.ts
@@ -3,8 +3,7 @@ import { index, route } from "@react-router/dev/routes";
 import { IS_DEVELOPMENT } from "../../shared/constants/root-env.constant";
 import { ROUTES_CONSTANTS } from "./constants/routes.constant";
 
-const { AUTH_PATHS, INDEX_FILE, ROUTE_PATHS } = ROUTES_CONSTANTS;
-const { SIGNIN, SIGNUP } = AUTH_PATHS;
+const { INDEX_FILE, ROUTE_PATHS } = ROUTES_CONSTANTS;
 const { API_DOCS, API_HEALTH, AUTH, CALENDAR, STAFF, TREATMENTS } = ROUTE_PATHS;
 
 // Health and documentation routes
@@ -12,10 +11,7 @@ const apiDocs = route(`/${API_DOCS}`, `pages/ApiDocs/${INDEX_FILE}`);
 const apiHealth = route(`/${API_HEALTH}`, `pages/ApiHealth/${INDEX_FILE}`);
 
 // Public routes
-const auth = route(
-  `/${AUTH}/:(${SIGNIN}|${SIGNUP})?`,
-  `pages/Auth/${INDEX_FILE}`,
-);
+const auth = route(`/${AUTH}/:step?`, `pages/Auth/${INDEX_FILE}`);
 const calendar = route(`/${CALENDAR}`, `pages/Calendar/${INDEX_FILE}`);
 const home = index(`pages/Home/${INDEX_FILE}`);
 const staff = route(`/${STAFF}`, `pages/Staff/${INDEX_FILE}`);
