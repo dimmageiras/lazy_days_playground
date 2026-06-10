bc02e2d1176fe536399f0b24f1a9368d3182035c
dimmageiras <dimmageiras@gmail.com>
2026-06-10 20:37:58 +0300

    refactor(shutdown): extract hot-reload handling into a helper

--- app/server/modules/shutdown/shutdown.module.ts
+++ app/server/modules/shutdown/shutdown.module.ts
@@ -1,18 +1,18 @@
 import closeWithGrace from "close-with-grace";
 
 import { BASE_URLS } from "@server/constants/base-urls.constant";
-import { ErrorHelper } from "@server/helpers/error.helper";
 import type { AppInstance } from "@server/types/instance.type";
 
 import { REDACT_PATHS } from "./constants/redact.constant";
 import { ClaimPortHelper } from "./helpers/claim-port.helper";
+import { HotReloadHelper } from "./helpers/hot-reload.helper";
 import { ShutdownHelper } from "./helpers/shutdown.helper";
 import { shutdownRoutes } from "./routes/shutdown.route";
 
 const { API_INTERNAL } = BASE_URLS;
 
 const { claimPort } = ClaimPortHelper;
-const { normalizeError } = ErrorHelper;
+const { acceptHotReload } = HotReloadHelper;
 const { buildShutdownHandler, buildShutdownOptions } = ShutdownHelper;
 
 const setupShutdown = async (
@@ -28,24 +28,7 @@ const setupShutdown = async (
     handle.uninstall();
   });
 
-  if (hot) {
-    const hotData: { instance?: AppInstance } = hot.data;
-
-    if (hotData.instance) {
-      try {
-        await hotData.instance.close();
-      } catch (rawError) {
-        instance.log.error(
-          normalizeError(rawError),
-          "💥 Failed to close the previous instance during hot reload",
-        );
-      }
-    }
-
-    hotData.instance = instance;
-
-    hot.accept();
-  }
+  await acceptHotReload(instance, hot);
 
   await instance.register(shutdownRoutes, {
     handle,
