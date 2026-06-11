456deb0568606409154a5c835aee737a56ca9d2a
dimmageiras <dimmageiras@gmail.com>
2026-06-11 18:48:07 +0300

    feat(bootstrap): dispose the server instance on dev hot reload

--- app/server/start.ts
+++ app/server/start.ts
@@ -39,9 +39,21 @@ let instance: AppInstance | undefined;
 try {
   instance = await buildApp(validatedEnv);
 
-  registerGracefulShutdown(instance);
+  const shutdownHandle = registerGracefulShutdown(instance);
 
   await instance.listen({ port: instance.appEnv.port });
+
+  if (import.meta.hot) {
+    const startedInstance = instance;
+
+    import.meta.hot.dispose(async () => {
+      shutdownHandle.uninstall();
+
+      await startedInstance.close();
+    });
+
+    import.meta.hot.accept();
+  }
 } catch (rawError) {
   const normalizedError = normalizeError(rawError);
 
