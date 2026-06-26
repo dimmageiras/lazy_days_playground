d6acbc262a663585ef1066533748534695c4efda
dimmageiras <dimmageiras@gmail.com>
2026-06-26 18:23:38 +0300

    refactor(app): implement start function in AppHelper and streamline server startup process

--- app/server/helpers/app.helper.ts
+++ app/server/helpers/app.helper.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 
 import { BASE_URLS } from "@server/constants/base-urls.constant";
+import type { Logger } from "@server/modules/logger";
 import type { BuildLoggerFunction } from "@server/modules/logger/types/logger.type";
 import type { SetupShutdownFunction } from "@server/modules/shutdown";
 import type { RedactPaths } from "@server/modules/shutdown/types/shutdown.type";
@@ -11,12 +12,14 @@ import { TIMING_IN_MS } from "@shared/constants/timing.constant";
 import type { ViteAppEnv } from "@shared/types/app-env.type";
 
 import { AppEnvHelper } from "./app-env.helper";
+import { EnvVarHelper } from "./env-var.helper";
 import { ErrorHelper } from "./error.helper";
 
 const { API_HEALTH } = BASE_URLS;
 const { SECONDS_TEN } = TIMING_IN_MS;
 
 const { buildAppEnv } = AppEnvHelper;
+const { isEnvValidationError, validateEnv } = EnvVarHelper;
 const { normalizeError, toError } = ErrorHelper;
 
 const build = async (
@@ -71,8 +74,88 @@ const build = async (
   }
 };
 
+const start = async (
+  env: ImportMetaEnv,
+  hot: ImportMeta["hot"],
+  modules: {
+    logger: {
+      buildFallbackLogger: () => Logger;
+      buildLogger: BuildLoggerFunction;
+    };
+    shutdown: {
+      redactPaths: RedactPaths;
+      setupShutdown: SetupShutdownFunction;
+    };
+    startup: {
+      claimPort: (instance: AppInstance) => Promise<void>;
+    };
+  },
+): Promise<void> => {
+  const {
+    logger: { buildFallbackLogger, buildLogger },
+    shutdown: { redactPaths, setupShutdown },
+    startup: { claimPort },
+  } = modules;
+
+  let validatedEnv: ViteAppEnv;
+
+  try {
+    validatedEnv = validateEnv(env);
+  } catch (error) {
+    const fallbackLogger = buildFallbackLogger();
+
+    if (isEnvValidationError(error)) {
+      fallbackLogger.fatal(
+        normalizeError(error),
+        "💥 Failed to validate the environment",
+      );
+    } else {
+      fallbackLogger.fatal(
+        normalizeError(error),
+        "💥 Unexpected error while validating the environment",
+      );
+    }
+
+    process.exit(1);
+  }
+
+  let instance: AppInstance | undefined;
+
+  try {
+    instance = await build(validatedEnv, hot, {
+      logger: { buildLogger },
+      shutdown: { redactPaths, setupShutdown },
+    });
+
+    await claimPort(instance);
+  } catch (rawError) {
+    const normalizedError = normalizeError(rawError);
+
+    if (!instance) {
+      const fallbackLogger = buildFallbackLogger();
+
+      fallbackLogger.fatal(normalizedError, "💥 Failed to start the server");
+
+      process.exit(1);
+    }
+
+    instance.log.fatal(normalizedError, "💥 Failed to start the server");
+
+    try {
+      await instance.close();
+    } catch (rawCloseError) {
+      instance.log.fatal(
+        normalizeError(rawCloseError),
+        "💥 Failed to close the server after a startup failure",
+      );
+    }
+
+    instance.log.flush(() => process.exit(1));
+  }
+};
+
 const AppHelper = Object.freeze({
-  build,
+  start,
 } as const);
 
 export { AppHelper };
--- app/server/start.ts
+++ app/server/start.ts
@@ -1,78 +1,12 @@
-import type { ViteAppEnv } from "@shared/types/app-env.type";
-
 import { AppHelper } from "./helpers/app.helper";
-import { EnvVarHelper } from "./helpers/env-var.helper";
-import { ErrorHelper } from "./helpers/error.helper";
 import { LoggerModule } from "./modules/logger";
 import { ShutdownModule } from "./modules/shutdown";
 import { StartupModule } from "./modules/startup";
-import type { AppInstance } from "./types/instance.type";
 
-const { build } = AppHelper;
-const { isEnvValidationError, validateEnv } = EnvVarHelper;
-const { normalizeError } = ErrorHelper;
+const { start } = AppHelper;
 
-const { buildFallbackLogger, buildLogger } = LoggerModule;
-const { redactPaths, setupShutdown } = ShutdownModule;
-const { claimPort } = StartupModule;
-
-let validatedEnv: ViteAppEnv;
-
-try {
-  validatedEnv = validateEnv(import.meta.env);
-} catch (error) {
-  const fallbackLogger = buildFallbackLogger();
-
-  if (isEnvValidationError(error)) {
-    fallbackLogger.fatal(
-      normalizeError(error),
-      "💥 Failed to validate the environment",
-    );
-  } else {
-    fallbackLogger.fatal(
-      normalizeError(error),
-      "💥 Unexpected error while validating the environment",
-    );
-  }
-
-  process.exit(1);
-}
-
-let instance: AppInstance | undefined;
-
-try {
-  instance = await build(validatedEnv, import.meta.hot, {
-    logger: {
-      buildLogger,
-    },
-    shutdown: {
-      redactPaths,
-      setupShutdown,
-    },
-  });
-
-  await claimPort(instance);
-} catch (rawError) {
-  const normalizedError = normalizeError(rawError);
-
-  if (!instance) {
-    const fallbackLogger = buildFallbackLogger();
-
-    fallbackLogger.fatal(normalizedError, "💥 Failed to start the server");
-
-    process.exit(1);
-  }
-
-  instance.log.fatal(normalizedError, "💥 Failed to start the server");
-
-  try {
-    await instance.close();
-  } catch (rawCloseError) {
-    instance.log.fatal(
-      normalizeError(rawCloseError),
-      "💥 Failed to close the server after a startup failure",
-    );
-  }
-
-  instance.log.flush(() => process.exit(1));
-}
+await start(import.meta.env, import.meta.hot, {
+  logger: LoggerModule,
+  shutdown: ShutdownModule,
+  startup: StartupModule,
+});
